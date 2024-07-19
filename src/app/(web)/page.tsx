169c9abb4f74cd2ec596edb06fbd7fdc763c1580
VladyslavVagin <vladtwince@gmail.com>
2024-07-19 20:00:00 +0200

    create room

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,13 +1,17 @@
+import FeaturedRoom from "@/components/FeaturedRoom/FeaturedRoom";
+import Gallery from "@/components/Gallery/Gallery";
 import HeroSection from "@/components/HeroSection/HeroSection";
+import NewsLetter from "@/components/NewsLetter/NewsLetter";
+import PageSearch from "@/components/PageSearch/PageSearch";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
-      {/* Page Search */}
-      {/* Featured Room */}
-      {/* Gallery */}
-      {/* Newsletter */}
+      <PageSearch />
+      <FeaturedRoom />
+      <Gallery />
+      <NewsLetter />
     </>
   );
 }
