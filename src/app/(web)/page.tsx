ca6cf42a6a844dd88ea8d58322b3fc8fd4fd7c04
VladyslavVagin <vladtwince@gmail.com>
2024-07-19 16:36:42 +0200

    search components

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <>
       <HeroSection />
+      {/* Page Search */}
+      {/* Featured Room */}
+      {/* Gallery */}
+      {/* Newsletter */}
     </>
   );
 }
--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,66 +1,8 @@
-import Image from "next/image";
+import ClientComponent from "./ClientComponent";
+import { heading1, section2 } from "./ServerComponent";
 
 const HeroSection = () => {
-  return (
-    <section className="flex px-4 items-center gap-12 container mx-auto">
-      <div className="py-10 h-full">
-        <h1 className="font-heading mb-6">Explore Our Exquisite Hotel</h1>
-        <p className="text-[#4a4a4a] dark:text-[#ffffff] mb-12 max-w-lg">
-          Experience an Exquisite Hotel Immersed in Rich History and Timeless
-          Elegance. Our Hotel Offers a Unique Blend of Modern Luxury and
-          Historic.
-        </p>
-        <button className="btn-primary">Get started</button>
-
-        <div className="flex justify-between mt-12">
-          <div className="flex gap-3 flex-col items-center justify-center">
-            <p className="text-xs lg:text-xl text-center">Basic Room</p>
-            <p className="md:font-bold font-medium text-lg xl:text-5xl">+20</p>
-          </div>
-          <div className="flex gap-3 flex-col items-center justify-center">
-            <p className="text-xs lg:text-xl text-center">Luxury Room</p>
-            <p className="md:font-bold font-medium text-lg xl:text-5xl">+20</p>
-          </div>
-          <div className="flex gap-3 flex-col items-center justify-center">
-            <p className="text-xs lg:text-xl text-center">Suite</p>
-            <p className="md:font-bold font-medium text-lg xl:text-5xl">+20</p>
-          </div>
-        </div>
-      </div>
-
-      <div className="md:grid hidden gap-8 grid-cols-1">
-        <div className="rounded-2xl overflow-hidden h-48">
-          <Image
-            src={"/images/hero-1.webp"}
-            alt="hero-1"
-            width={300}
-            height={300}
-            className="img scale-animation"
-          />
-        </div>
-        <div className="grid grid-cols-2 gap-8 h-48">
-          <div className="rounded-2xl overflow-hidden">
-            <Image
-              src={"/images/hero-2.webp"}
-              alt="hero-2"
-              width={300}
-              height={300}
-              className="img scale-animation"
-            />
-          </div>
-          <div className="rounded-2xl overflow-hidden">
-            <Image
-              src={"/images/hero-3.webp"}
-              alt="hero-3"
-              width={300}
-              height={300}
-              className="img scale-animation"
-            />
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+  return <ClientComponent heading1={heading1} section2={section2} />;
 };
 
 export default HeroSection;
