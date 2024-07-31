56502ddf679a4a16937e8a6d19d07f8c7bffb982
VladyslavVagin <vladtwince@gmail.com>
2024-07-31 23:07:42 +0200

    fix error.tsx

--- src/app/(web)/error.tsx
+++ src/app/(web)/error.tsx
@@ -1,23 +1,19 @@
-"use client";
+'use client';
 
-import Error from "next/error";
-import React from "react";
-
-const Error = ({
+export default function Error({
   error,
   reset,
 }: {
   error: Error & { digest?: string };
   reset: () => void;
-}) => {
+}) {
   return (
-    <div className="container mx-auto">
-      <h2 className="font-heading text-red-800 mb-10">Something went wrong!</h2>
-      <button className="btn-primary" onClick={() => reset()}>
+    <div className='container mx-auto'>
+      <h2 className='font-heading text-red-800 mb-10'>Something went wrong!</h2>
+
+      <button onClick={() => reset()} className='btn-primary'>
         Try Again
       </button>
     </div>
   );
-};
-
-export default Error;
+}
