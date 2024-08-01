b7beda47759a21bec73296aef00363ba7c69750f
VladyslavVagin <vladtwince@gmail.com>
2024-08-01 08:06:38 +0200

    update

--- src/libs/apis.ts
+++ src/libs/apis.ts
@@ -1,9 +1,9 @@
+import axios from "axios";
 import { CreateBookingDto, Room } from "@/models/room";
 import sanityClient from "./sanity";
 import * as queries from "./sanityQueries";
 import { Booking } from "@/models/booking";
 import { CreateReviewDto, Review, UpdateReviewDto } from "@/models/review";
-import hotelRoom from "../../schemaTypes/hotelRoom";
 
 export async function getFeaturedRoom() {
   const result = await sanityClient.fetch<Room>(
